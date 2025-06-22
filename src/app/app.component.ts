d6cf79fb41454e848f5fe5f33c136e35c356efe4
Natanael A. de Morais <natanaelallves@gmail.com>
2025-06-21 22:06:58 -0300

    pok-01: style: Styling app header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonSearchbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonContent,
+  IonFooter
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonGrid, IonRow, IonCol, IonContent, IonFooter],
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,14 +3,13 @@ import {
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonContent,
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle],
 })
 export class HomePage {
   constructor() {}
