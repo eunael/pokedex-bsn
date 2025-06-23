e5a7bc041f9b5a5a4db8dd374edffee92550be62
Natanael A. de Morais <natanaelallves@gmail.com>
2025-06-23 17:26:37 -0300

    pok-03: chore: Remove layout components from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonContent,
-} from '@ionic/angular/standalone';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [
-    IonApp,
-    IonRouterOutlet,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonContent,
-    SearchbarComponent,
-  ],
+  imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor(private router: Router) {}
-
-  redirectToHome() {
-    this.router.navigate([`/`]);
-  }
+  constructor() {}
 }
