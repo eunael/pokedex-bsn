2bd1b6a6e38c3c66d5597e223b631dd606806c89
Natanael A. de Morais <natanaelallves@gmail.com>
2025-06-24 00:05:08 -0300

    pok-04: feat: Building details page

--- src/app/pokemon-details/pokemon-details.page.ts
+++ src/app/pokemon-details/pokemon-details.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
   IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonCardSubtitle,
 } from '@ionic/angular/standalone';
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { ActivatedRoute } from '@angular/router';
+import { SearchService } from '../services/search.service';
+import { Pokemon } from '../types/Pokemon';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -15,13 +31,33 @@ import {
   standalone: true,
   imports: [
     IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
     CommonModule,
     FormsModule,
+    ToolbarComponent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonCardSubtitle,
   ],
 })
-export class PokemonDetailsPage {
-  constructor() {}
+export class PokemonDetailsPage implements OnInit {
+  search = inject(SearchService);
+  pokemon: WritableSignal<Pokemon | null> = signal(null);
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.search.searchPokemonByIdOrName(id).subscribe({
+      next: (pokemon: Pokemon) => {
+        this.pokemon.set(pokemon);
+      },
+    });
+  }
 }
